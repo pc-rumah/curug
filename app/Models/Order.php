dc1c19708866f53746152d1932e0df595d9b5521
pc-rumah <wow23787@gmail.com>
2025-05-27 07:06:50 +0700

    ubah agar tiket id lebih unik

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,19 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
     use HasFactory;
 
-    protected $guarded = [];
+    protected $guarded = ['id'];
 
     public static function generateOrderCode()
     {
-        $latestOrder = self::latest('id')->first();
-        $number = $latestOrder ? (intval(substr($latestOrder->order_code, -4)) + 1) : 1;
-        return 'tiket-' . str_pad($number, 4, '0', STR_PAD_LEFT);
+        $timestamp = now()->format('YmdHis'); // contoh: 20250527150130
+        $random = strtoupper(Str::random(4)); // contoh: A9Z3
+        return 'tiket-' . $timestamp . '-' . $random;
     }
 }
